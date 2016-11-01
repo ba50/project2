af44e44268eb5f123625bffac208380fe6f478a7
Bartek <barjas02@gmail.com>
2016-11-01 17:42:05 +0100

    update

--- project2/dane_stat.hpp
+++ project2/dane_stat.hpp
@@ -1,24 +1,44 @@
-#ifndef _dane_stat_hpp_
-#define _dane_stat_hpp_
+#pragma once
+
 #include <string>
 #include <vector>
+#include <memory>
+
+
 class DaneStat // klasa bazowa
 {
+
 public:
 	DaneStat(const std::string &nazwa);
+
 	// zwraca referencje na przechowywane dane
 	virtual const std::vector <float> &dane() const = 0;
 	virtual ~DaneStat() {};
 	virtual const std::string &nazwa() const; // zwraca nazwe pliku
+
 protected:
 	std::string nazwa_; // nazwa pliku
 };
+
+
 // proxy - wczytuje prawdziwy obiekt przy pierwszym u¿yciu
 class DaneStatProxy : public DaneStat
 {
+	std::shared_ptr<DaneStat> dane_;
+public:
+	DaneStatProxy(const std::string nazwa) : DaneStat(nazwa), dane_(nullptr)
+	{};
+	~DaneStatProxy() {};
+	virtual const std::vector <float> &dane() const {
+		if (!dane_) dane_ = std::make_shared<DaneStat> DaneStatReal(nazwa_);
+	}
 };
+
+
 // "prawdziwy" obiekt przechowujacy dane
 class DaneStatReal : public DaneStat
 {
+
+public:
+	DaneStatReal(const std::string nazwa) : DaneStat(nazwa) {};
 };
-#endif
--- project2/rozklad.hpp
+++ project2/rozklad.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <map>
 #include <vector>
 
@@ -8,10 +10,12 @@ typedef std::map <std::string, float> ParametryRozkladu;
 
 class Rozklad // ATD - klasa bazowa dla "obliczaczy" estymatorow
 {
+
 public:
 	// ustawia referencje na dane do analizy
-	explicit Rozklad(const std::vector <float> &dane);
-	virtual ~Rozklad() {}
+	explicit Rozklad(const std::vector <float> &dane) :
+	dane_(dane) {};
+	virtual ~Rozklad() {};
 	// oblicza estymatory i zwraca je w mapie
 	virtual std::auto_ptr <ParametryRozkladu> oblicz() const = 0;
 
@@ -23,14 +27,21 @@ protected:
 
 class RozkladGaussa : public Rozklad
 {
+
 public:
 	// nie robi nic sensownego poza wywolaniem konstr. klasy bazowej z odpowiednim parametrem
-	explicit RozkladGaussa(const std::vector <float> &dane);
-	virtual ~RozkladGaussa() {}
+	explicit RozkladGaussa(const std::vector <float> &dane) :
+	Rozklad(dane) {};
+	~RozkladGaussa() {};
 	// liczy wartosc srednia i odchylenie standardowe
-	virtual std::auto_ptr <ParametryRozkladu> oblicz() const;
+	std::auto_ptr <ParametryRozkladu> oblicz() const override {
+		return std::auto_ptr<ParametryRozkladu>();
+	}
+
 	// statyczna met. tworzaca i zwracajaca wskaznik na obiekt wlasnego typu
-	static Rozklad* kreator(const std::vector <float> &dane);
+	static Rozklad* kreator(const std::vector <float> &dane) {
+		return new RozkladGaussa(dane);
+	};
 };
 
 
@@ -41,18 +52,34 @@ typedef Rozklad* (*KreatorRozkladu)(const std::vector <float> &);
 
 class FabrykaRozkladow // FABRYKA! :)
 {
+
 private:
 	// przechowuje wskaüniki kreatorow (funkcji tworzπcych!)
 	static std::map <unsigned, KreatorRozkladu> rozklady;
 	// przechowuje nazwy rozkladow
 	static std::map <unsigned, std::string> nazwy;
+	//rejestruje kolejne rozklady
+	static unsigned id;
+
 public:
 	// rejestruje kreator danego rozkladu (id generowane przyrostowo od 1)
-	static void rejestruj(KreatorRozkladu kr, const std::string &nazwa);
+	static void rejestruj(KreatorRozkladu kr, const std::string &nazwa) {
+		nazwy[id] = nazwa;
+		rozklady[id] = kr;
+		id++;
+	}
 	// wola kreator dla rozkladu o wybranym id
-	static Rozklad *utworz(unsigned id, const std::vector <float> &dane);
+	static Rozklad *utworz(unsigned id, const std::vector <float> &dane) {
+		return rozklady[id](dane);
+	}
 	// zwraca nazwe rozkladu o identyfikatorze id
-	static std::string nazwa(unsigned id);
+	static std::string nazwa(unsigned id) {
+		return nazwy[id];
+	}
 	// zwraca liczbe zarejestrowanych rozkladow
-	static unsigned ilosc() { return rozklady.size(); }
+	static unsigned ilosc(){
+		return static_cast<unsigned>(rozklady.size());
+	}
 };
+
+unsigned FabrykaRozkladow::id = 0;
